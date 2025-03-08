349e534171b4e076fa344cae583008021dba3f0b
NaotoMakimoto <makimaki2510@gmail.com>
2025-03-08 22:42:02 +0900

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,16 +1,24 @@
-export default function middleware(req) {
-    const auth = req.headers.get("authorization");
-    const expectedAuth = "Basic " + btoa("missiona:kadai");
-  
-    if (auth !== expectedAuth) {
-      return new Response("Unauthorized", {
-        status: 401,
-        headers: {
-          "WWW-Authenticate": 'Basic realm="Secure Area"',
-        },
-      });
-    }
-  
-    return Response.next();
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const auth = req.headers.get("authorization");
+
+  const username = "missiona";
+  const password = "kadai";
+  const expectedAuth = "Basic " + btoa(username + ":" + password);
+
+  if (auth !== expectedAuth) {
+    return new NextResponse("Unauthorized", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Secure Area"',
+      },
+    });
   }
-  
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/:path*",
+};
