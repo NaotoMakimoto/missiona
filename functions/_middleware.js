35f581cbd221449fb84e370311835fcfe9c335e6
NaotoMakimoto <makimaki2510@gmail.com>
2025-03-10 23:12:28 +0900

    update

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,22 +1,23 @@
+import { NextResponse } from 'next/server';
+
 export const onRequest = async ({ request }) => {
-    const username = "missiona";
-    const password = "kadai";
-  
-    const authHeader = request.headers.get("Authorization");
-    if (authHeader) {
-      const encoded = authHeader.split(" ")[1];
-      const decoded = atob(encoded);
-      const [user, pass] = decoded.split(":");
-      if (user === username && pass === password) {
-        return; // 認証OKならそのまま続行
-      }
+  const username = "missiona";
+  const password = "kadai";
+
+  const authHeader = request.headers.get("Authorization");
+  if (authHeader) {
+    const encoded = authHeader.split(" ")[1];
+    const decoded = atob(encoded);
+    const [user, pass] = decoded.split(":");
+    if (user === username && pass === password) {
+      return NextResponse.next();  
     }
-  
-    return new Response("Unauthorized", {
-      status: 401,
-      headers: {
-        "WWW-Authenticate": 'Basic realm="Protected Area"',
-      },
-    });
-  };
-  
+  }
+
+  return new Response("Unauthorized", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Protected Area"',
+    },
+  });
+};
